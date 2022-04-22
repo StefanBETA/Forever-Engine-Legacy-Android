86848cee437048d50db43dde43eeb40a05b0c852
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2022-04-22 23:12:02 +0300

    Update OptionsMenuState.hx

--- source/meta/state/menus/OptionsMenuState.hx
+++ source/meta/state/menus/OptionsMenuState.hx
@@ -62,6 +62,7 @@ class OptionsMenuState extends MusicBeatState
 				[
 					['Game Settings', null],
 					['', null],
+					['Controller Mode', getFromOption],
 					['Downscroll', getFromOption],
 					['Centered Notefield', getFromOption],
 					['Ghost Tapping', getFromOption],
